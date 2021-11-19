581f7ca74a27f6ff0b1b83ba194c1f8a5e31a3b6
Nizamuddin4493 <nizamuddin4493@gmail.com>
2021-11-20 02:04:41 +0430

    Add linter and Error message color change

--- js/script.js
+++ js/script.js
@@ -1,49 +1,50 @@
-let menu = document.querySelector(".burger-icon");
-let menuBody = document.querySelector(".menu")
-let close = document.querySelector(".close_btn");
+const menu = document.querySelector('.burger-icon');
+const menuBody = document.querySelector('.menu');
+const close = document.querySelector('.close_btn');
 
-let portfolio = document.querySelector("#portfolio");
-let about = document.querySelector("#about");
-let contact = document.querySelector("#contact");
+const portfolio = document.querySelector('#portfolio');
+const about = document.querySelector('#about');
+const contact = document.querySelector('#contact');
 
-menu.addEventListener("click", () => {
-    close_menu();
-});
-
-close.addEventListener("click", () => {
-    close_menu();
-});
-
-portfolio.addEventListener("click", () => {
-    close_menu();
-});
-
-about.addEventListener("click", () => {
-    close_menu();
-});
-
-contact.addEventListener("click", () => {
-    close_menu();
-});
-
-const close_menu = () => {
-    menuBody.classList.toggle("invisible");
+const closeMenu = () => {
+  menuBody.classList.toggle('invisible');
 };
+
+menu.addEventListener('click', () => {
+  closeMenu();
+});
+
+close.addEventListener('click', () => {
+  closeMenu();
+});
+
+portfolio.addEventListener('click', () => {
+  closeMenu();
+});
+
+about.addEventListener('click', () => {
+  closeMenu();
+});
+
+contact.addEventListener('click', () => {
+  closeMenu();
+});
 
 // Code for Form Validation
 
-const setError =  (error) => {
-  let element = document.querySelector('.formError').innerHTML = error;
-}
+const setError = (error) => {
+  document.querySelector('.formError').innerHTML = error;
+};
 
 const validateForm = () => {
   let returnVal = true;
-  let email = document.forms['portfolioForm']['email-address'].value;
+  const email = document.querySelector('#f-email').value;
   if (email === email.toLowerCase()) {
     return returnVal;
-  } else {
+  }
+  if (email !== email.toLowerCase()) {
     setError('* Please make sure the Email is in Lower Case');
     returnVal = false;
   }
   return returnVal;
-}
+};
