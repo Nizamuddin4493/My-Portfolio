2e6f656f438cecb9cc1d5721f253ca5f9840f9be
Nizamuddin4493 <nizamuddin4493@gmail.com>
2021-11-20 05:36:27 +0430

    Popup window created card dynamically

--- js/script.js
+++ js/script.js
@@ -1,17 +1,44 @@
+const popupArr = [
+  {
+    title: 'Data Dashboard healthcare',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    piture: './images/Tags.png',
+  },
+  {
+    title: 'Data Dashboard healthcare',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    piture: './images/Tags.png',
+  },
+  {
+    title: 'Awesome Website Portfolio Design',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    piture: './images/Tags.png',
+  },
+  {
+    title: 'Profesional Art Printing Data More',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    piture: './images/Tags.png',
+  },
+  {
+    title: 'Data Dashboard healthcare',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    piture: './images/Tags.png',
+  },
+  {
+    title: 'Awesome Website Portfolio Design',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    piture: './images/Tags.png',
+  },
+];
+
+const cartsContainer = document.querySelector('#cartsContainer');
 const menu = document.querySelector('.burger-icon');
-
 const menuBody = document.querySelector('.menu');
-
 const close = document.querySelector('.close_btn');
-
 const clsoePopupBtn = document.querySelector('.popup_close');
-
 const popup = document.querySelector('.popup');
-
 const portfolio = document.querySelector('#portfolio');
-
 const about = document.querySelector('#about');
-
 const contact = document.querySelector('#contact');
 
 const closeMenu = () => {
@@ -44,4 +71,30 @@ contact.addEventListener('click', () => {
 
 clsoePopupBtn.addEventListener('click', () => {
   closePopup();
+});
+
+const popelement = (cartDesign) => {
+  const cardDiv = document.createElement('div');
+  const cardH2 = document.createElement('h2');
+  const cardP = document.createElement('p');
+  const cardDiv2 = document.createElement('div');
+  const cardImg = document.createElement('img');
+  const cardBtn = document.createElement('button');
+  cardDiv.classList = 'cart-2';
+  cardBtn.classList = 'card-btn-2';
+  cardH2.textContent = cartDesign.title;
+  cardP.textContent = cartDesign.description;
+  cardImg.srcset = cartDesign.piture;
+  cardBtn.textContent = 'See Project';
+  cardBtn.setAttribute('onclick', 'closePopup()');
+  cardDiv.appendChild(cardH2);
+  cardDiv.appendChild(cardP);
+  cardDiv.appendChild(cardDiv2);
+  cardDiv.appendChild(cardBtn);
+  cardDiv2.appendChild(cardImg);
+  cartsContainer.appendChild(cardDiv);
+};
+
+popupArr.forEach((element) => {
+  popelement(element);
 });
